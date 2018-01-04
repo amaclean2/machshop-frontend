f863b9ea694535d2528511d0dc92c518207f5be2
amaclean <andrew.n.maclean@gmail.com>
2018-01-03 18:21:33 -0800

    fixes

--- src/Components/Pages/Operations/AdminOperation.js
+++ src/Components/Pages/Operations/AdminOperation.js
@@ -8,15 +8,15 @@ class AdminOperation extends Component {
     if(!this.props.edit) {
       return (
         <div className='machine-operation op-meta-data'>
-          <DescriptionItem header={'Creator: '} value={this.props.data.stationData.creator} />
-          <DescriptionItem header={'Time Spent: '} value={this.props.data.stationData.time_spent} />
+          <DescriptionItem header={'Creator: '} value={this.props.data.station_data.creator} />
+          <DescriptionItem header={'Time Spent: '} value={this.props.data.station_data.time_spent} />
         </div>
       )
     } else {
       return (
         <div className='machine-operation op-meta-data'>
-          <EditableItem header={'Creator: '} value={this.props.data.stationData.creator}  change={this.props.change} name={'creator'}/>
-          <EditableItem header={'Time Spent: '} value={this.props.data.stationData.time_spent}  change={this.props.change} name={'time_spent'}/>
+          <EditableItem header={'Creator: '} value={this.props.data.station_data.creator}  change={this.props.change} name={'creator'}/>
+          <EditableItem header={'Time Spent: '} value={this.props.data.station_data.time_spent}  change={this.props.change} name={'time_spent'}/>
         </div>
       )
     }
--- src/Components/Pages/Operations/MillOperation.js
+++ src/Components/Pages/Operations/MillOperation.js
@@ -8,23 +8,23 @@ class MillOperation extends Component {
     if(!this.props.edit) {
       return (
         <div className='machine-operation op-meta-data'>
-          <DescriptionItem header={'Machine Number: '} value={this.props.data.stationData.machine_number} />
-          <DescriptionItem header={'Operator: '} value={this.props.data.stationData.operator} />
-          <DescriptionItem header={'Original Part Count: '} value={this.props.data.stationData.start_count} />
-          <DescriptionItem header={'Finished Part Count: '} value={this.props.data.stationData.finish_count} />
-          <DescriptionItem header={'Setup Time: '} value={this.props.data.stationData.setup_time} />
-          <DescriptionItem header={'Run Time: '} value={this.props.data.stationData.run_time} />
+          <DescriptionItem header={'Machine Number: '} value={this.props.data.station_data.machine_number} />
+          <DescriptionItem header={'Operator: '} value={this.props.data.station_data.operator} />
+          <DescriptionItem header={'Original Part Count: '} value={this.props.data.station_data.start_count} />
+          <DescriptionItem header={'Finished Part Count: '} value={this.props.data.station_data.finish_count} />
+          <DescriptionItem header={'Setup Time: '} value={this.props.data.station_data.setup_time} />
+          <DescriptionItem header={'Run Time: '} value={this.props.data.station_data.run_time} />
         </div>
       )
     } else {
       return (
         <div className='machine-operation op-meta-data'>
-          <EditableItem header={'Machine Number: '} value={this.props.data.stationData.machine_number}  change={this.props.change} name={'machine_number'} type={'number'}/>
-          <EditableItem header={'Operator: '} value={this.props.data.stationData.operator}  change={this.props.change} name={'operator'}/>
-          <EditableItem header={'Original Part Count: '} value={this.props.data.stationData.start_count}  change={this.props.change} name={'start_count'} type={'number'}/>
-          <EditableItem header={'Finished Part Count: '} value={this.props.data.stationData.finish_count}  change={this.props.change} name={'finish_count'} type={'number'}/>
-          <EditableItem header={'Setup Time: '} value={this.props.data.stationData.setup_time}  change={this.props.change} name={'setup_time'}/>
-          <EditableItem header={'Run Time: '} value={this.props.data.stationData.run_time}  change={this.props.change} name={'run_time'}/>
+          <EditableItem header={'Machine Number: '} value={this.props.data.station_data.machine_number}  change={this.props.change} name={'machine_number'} type={'number'}/>
+          <EditableItem header={'Operator: '} value={this.props.data.station_data.operator}  change={this.props.change} name={'operator'}/>
+          <EditableItem header={'Original Part Count: '} value={this.props.data.station_data.start_count}  change={this.props.change} name={'start_count'} type={'number'}/>
+          <EditableItem header={'Finished Part Count: '} value={this.props.data.station_data.finish_count}  change={this.props.change} name={'finish_count'} type={'number'}/>
+          <EditableItem header={'Setup Time: '} value={this.props.data.station_data.setup_time}  change={this.props.change} name={'setup_time'}/>
+          <EditableItem header={'Run Time: '} value={this.props.data.station_data.run_time}  change={this.props.change} name={'run_time'}/>
           <div className='button-bar'><button className='button small-button'>Add Setup Sheet</button></div>
         </div>
       )
