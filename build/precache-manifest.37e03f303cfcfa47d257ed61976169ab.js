8e66a541795ed055a962697edbc178fc46cc5f3a
Andrew MacLean <andrewmaclean@DreamBook.local>
2018-11-15 15:45:47 +0000

    production build

---
+++ build/precache-manifest.37e03f303cfcfa47d257ed61976169ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56a40e2c1a0a3cddbdd8",
+    "url": "/static/js/main.56a40e2c.chunk.js"
+  },
+  {
+    "revision": "8a52ce70a18a65fbed10",
+    "url": "/static/js/1.8a52ce70.chunk.js"
+  },
+  {
+    "revision": "56a40e2c1a0a3cddbdd8",
+    "url": "/static/css/main.3a85afa9.chunk.css"
+  },
+  {
+    "revision": "6d5813339e406a42ebca8924a0bb8ebc",
+    "url": "/index.html"
+  }
+];
