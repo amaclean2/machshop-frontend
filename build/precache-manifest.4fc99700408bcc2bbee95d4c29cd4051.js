67024600248e33df1f581247dcecfb9767f2db7d
amaclean <andrew.n.maclean@gmail.com>
2019-01-02 15:28:41 -0800

    firebase tools

---
+++ build/precache-manifest.4fc99700408bcc2bbee95d4c29cd4051.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "06411d23dc25eb7a8988",
+    "url": "/static/js/main.06411d23.chunk.js"
+  },
+  {
+    "revision": "2611507b5c9f17880592",
+    "url": "/static/js/1.2611507b.chunk.js"
+  },
+  {
+    "revision": "06411d23dc25eb7a8988",
+    "url": "/static/css/main.864c02ea.chunk.css"
+  },
+  {
+    "revision": "e81ca2722573e81ce75e37d603c2d3bf",
+    "url": "/index.html"
+  }
+];
